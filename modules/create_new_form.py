b924924e4caee797ef0f1334bf269615fc855f35
KiD <vietnase62219@fpt.edu.vn>
2019-08-11 02:05:04 +0700

    04.getDataForForms Create form HTML

--- modules/create_new_form.py
+++ modules/create_new_form.py
@@ -2,5 +2,5 @@
 from flask import Flask, render_template, json, request, jsonify, session
 
 def create_new_form():
-    form_name = ["Full name", "Gender", "Date of birth", "Country of birth"]
-    return render_template('index.html', form_name=form_name)
+    form_names = ["Full name", "Gender", "Date of birth", "Country of birth"]
+    return render_template('index.html', form_names=form_names)
